7ea26fbb1669cdadb3e8f63e980854e052e8a439
Vinicius Lourenco <vinicius_lourenco@outlook.com>
2025-07-31 07:00:33 -0300

    test: implement monthly summary by page title test

--- src/main/java/br/sp/vinilourenco/tests/ResumoTest.java
+++ src/main/java/br/sp/vinilourenco/tests/ResumoTest.java
@@ -1,10 +1,13 @@
 package br.sp.vinilourenco.tests;
 
 import br.sp.vinilourenco.core.BaseTest;
+import br.sp.vinilourenco.core.DriverFactory;
 import br.sp.vinilourenco.pages.MenuPage;
 import br.sp.vinilourenco.pages.ResumoPage;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static br.sp.vinilourenco.core.DriverFactory.*;
 
 public class ResumoTest extends BaseTest {
     private MenuPage menuPage= new MenuPage();
@@ -16,5 +19,11 @@ public class ResumoTest extends BaseTest {
         resumoPage.excluirMovimentacao();
         Assert.assertEquals("Movimentação removida com sucesso!",
                 resumoPage.obterMensagemSucesso());
+    }
+
+    @Test
+    public void testResumoMensal() {
+        menuPage.acessarTelaResumo();
+        Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
     }
 }
